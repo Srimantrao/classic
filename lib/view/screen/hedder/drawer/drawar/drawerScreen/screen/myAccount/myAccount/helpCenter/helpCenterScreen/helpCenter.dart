e90e5f4dd8b61986fec8432eea3de73fb8defd4d
Srimantrao <141737566+Srimantrao@users.noreply.github.com>
2026-04-14 13:12:51 +0530

    classic

--- lib/view/screen/hedder/drawer/drawar/drawerScreen/screen/myAccount/myAccount/helpCenter/helpCenterScreen/helpCenter.dart
+++ lib/view/screen/hedder/drawer/drawar/drawerScreen/screen/myAccount/myAccount/helpCenter/helpCenterScreen/helpCenter.dart
@@ -15,7 +15,6 @@ import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 
 class HelpCenter extends StatelessWidget {
   final helpcenterUI = Get.put(HelpcenterController());
-
   @override
   Widget build(BuildContext context) {
     return Fullscreen(
